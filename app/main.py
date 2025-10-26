1afe8abbf5a67b44aed4e9128efa93ea2251ca60
arief <ariefshaik001@gmail.com>
2025-10-26 17:25:01 +0530

    feat: add alembic db migrations

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.db.session import engine
 from app.models.models import Base
 from app.routes import post_routes
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
-    await engine.dispose()
+    
 
 
 app = FastAPI(
--- app/models/models.py
+++ app/models/models.py
@@ -1,13 +1,38 @@
 from typing import override
-from sqlalchemy import Integer, String, Text, DateTime, func
+from sqlalchemy import Boolean, ForeignKey, Integer, String, Text, DateTime, func
 from app.db.session import Base
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from datetime import datetime
+
+
+class User(Base):
+    """
+    Model for User
+    """
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    posts: Mapped[list["Post"]] = relationship(
+        "Posts", back_populates="owner", cascade="all, delete-orphan"
+    )
+
+    # Dunder Method / Magic Method
+    @override
+    def __repr__(self) -> str:
+        return f"<User(email='{self.email}')>"
 
 
 class Post(Base):
     """
-    Model for Blog Post
+    Model for Post
     """
 
     __tablename__ = "posts"
@@ -19,9 +44,13 @@ class Post(Base):
         DateTime(timezone=True), server_default=func.now()
     )
 
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
+    owner: Mapped["User"] = relationship("User", back_populates="posts")
+
     # Dunder Method / Magic Method
     @override
     def __repr__(self) -> str:
         return f"<Post(id={self.id!r}, title={self.title!r})>"
-
-
