bbee88aee0bf6c4b32ec8d1d53ef6b80ad2d1ce8
arief <ariefshaik001@gmail.com>
2025-10-27 22:24:09 +0530

    chore: add CORS

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
+from multiprocessing import allow_connection_pickling
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes import post_routes, user_routes, auth_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
-    
 
 
 app = FastAPI(
     title="API",
     description="Backend API for a technical blog platform.",
     lifespan=lifespan,
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_routes.router, tags=["Authentication"])
